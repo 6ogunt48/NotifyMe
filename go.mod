cdeba078914e6fd56151d9ca3f6e7c89e97e9a07
6ogunt48 <114788338+6ogunt48@users.noreply.github.com>
2024-06-05 00:08:46 +0100

    added structs for toml data type

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/6ogunt48/checkemailbot
 
 go 1.22
-
-require github.com/emersion/go-imap/v2 v2.0.0-beta.3 // indirect
--- main.go
+++ main.go
@@ -8,3 +8,22 @@ func main() {
 	fmt.Println("hello world")
 
 }
+
+type logindetails struct {
+	server   string
+	port     int16
+	username string
+	password string
+}
+
+func loadTOML() {
+
+}
+
+func verifyIMAP() {
+
+}
+
+func checkMail() {
+
+}
