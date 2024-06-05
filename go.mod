cedd04e7fce2fe4e99588aa0b533ef9076f31d8d
6ogunt48 <114788338+6ogunt48@users.noreply.github.com>
2024-06-05 17:57:37 +0100

    added code to decode toml and read toml

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/6ogunt48/checkemailbot
 
 go 1.22
+
+require github.com/BurntSushi/toml v1.4.0 // indirect
--- main.go
+++ main.go
@@ -2,21 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/BurntSushi/toml"
+	"log"
+	"os"
 )
 
-type logindetails struct {
-	server   string
-	port     int16
-	username string
-	password string
+type Config struct {
+	LoginDetails []struct {
+		server   string
+		port     int16
+		username string
+		password string
+	}
 }
 
 func main() {
 	fmt.Println("hello world")
-
-}
-
-func loadTOML() {
 }
 
 func verifyIMAP() {
@@ -24,3 +25,21 @@ func verifyIMAP() {
 }
 
 func checkMail() {}
+
+func readTOML() []byte {
+	data, err := os.ReadFile("config.toml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return data
+}
+
+func decodeTOML() Config {
+	var config Config
+	data := readTOML()
+	_, err := toml.Decode(string(data), &config)
+	if err != nil {
+		log.Fatal("Error decoding TOML:", err)
+	}
+	return config
+}
